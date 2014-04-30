ca3f83648581c795a4b2e48f1929568250c4c6e0
Sergio <sergiorsiqueira9@gmail.com>
2014-04-30 18:21:34 -0300

    exemplo de ano bissexto com node.js

---
+++ bissexto.js
@@ -0,0 +1,17 @@
+var readline = require('readline');
+
+var r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+r1.question('Digite um ano:', function(year) {
+
+  if (((year % 4) == 0) && ((year % 100) != 0) || ((year % 400) == 0)) {
+    console.log('O ano '+year+' é bissexto');
+  } else {
+    console.log('O ano '+year+' não é bissexto');
+  }
+  
+  r1.close();
+});
